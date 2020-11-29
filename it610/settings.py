410b16cc1da597f018e9eb6bc1289904030c2621
ningcaichen <zouxin@tapindata.com>
2020-11-29 16:55:51 +0800

    修改抓取规则

--- it610/settings.py
+++ it610/settings.py
@@ -13,48 +13,48 @@ BOT_NAME = 'it610'
 
 SPIDER_MODULES = ['it610.spiders']
 NEWSPIDER_MODULE = 'it610.spiders'
-
+DOWNLOAD_DELAY = 3
 DEFAULT_REQUEST_HEADERS = {
-    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-    'Connection': 'close',
-    'referer': 'https://www.it610.com/'
+	'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+	'Connection': 'close',
+	'referer': 'https://www.it610.com/'
 }
 UA_LIST = [
-    "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; AcooBrowser; .NET CLR 1.1.4322; .NET CLR 2.0.50727)",
-    "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0; Acoo Browser; SLCC1; .NET CLR 2.0.50727; Media Center PC 5.0; .NET CLR 3.0.04506)",
-    "Mozilla/4.0 (compatible; MSIE 7.0; AOL 9.5; AOLBuild 4337.35; Windows NT 5.1; .NET CLR 1.1.4322; .NET CLR 2.0.50727)",
-    "Mozilla/5.0 (Windows; U; MSIE 9.0; Windows NT 9.0; en-US)",
-    "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Win64; x64; Trident/5.0; .NET CLR 3.5.30729; .NET CLR 3.0.30729; .NET CLR 2.0.50727; Media Center PC 6.0)",
-    "Mozilla/5.0 (compatible; MSIE 8.0; Windows NT 6.0; Trident/4.0; WOW64; Trident/4.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; .NET CLR 1.0.3705; .NET CLR 1.1.4322)",
-    "Mozilla/4.0 (compatible; MSIE 7.0b; Windows NT 5.2; .NET CLR 1.1.4322; .NET CLR 2.0.50727; InfoPath.2; .NET CLR 3.0.04506.30)",
-    "Mozilla/5.0 (Windows; U; Windows NT 5.1; zh-CN) AppleWebKit/523.15 (KHTML, like Gecko, Safari/419.3) Arora/0.3 (Change: 287 c9dfb30)",
-    "Mozilla/5.0 (X11; U; Linux; en-US) AppleWebKit/527+ (KHTML, like Gecko, Safari/419.3) Arora/0.6",
-    "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.2pre) Gecko/20070215 K-Ninja/2.1.1",
-    "Mozilla/5.0 (Windows; U; Windows NT 5.1; zh-CN; rv:1.9) Gecko/20080705 Firefox/3.0 Kapiko/3.0",
-    "Mozilla/5.0 (X11; Linux i686; U;) Gecko/20070322 Kazehakase/0.4.5",
-    "Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.8) Gecko Fedora/1.9.0.8-1.fc10 Kazehakase/0.5.6",
-    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.11 (KHTML, like Gecko) Chrome/17.0.963.56 Safari/535.11",
-    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_3) AppleWebKit/535.20 (KHTML, like Gecko) Chrome/19.0.1036.7 Safari/535.20",
-    "Opera/9.80 (Macintosh; Intel Mac OS X 10.6.8; U; fr) Presto/2.9.168 Version/11.52",
-    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.11 (KHTML, like Gecko) Chrome/20.0.1132.11 TaoBrowser/2.0 Safari/536.11",
-    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/21.0.1180.71 Safari/537.1 LBBROWSER",
-    "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; WOW64; Trident/5.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; .NET4.0C; .NET4.0E; LBBROWSER)",
-    "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; QQDownload 732; .NET4.0C; .NET4.0E; LBBROWSER)",
-    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.11 (KHTML, like Gecko) Chrome/17.0.963.84 Safari/535.11 LBBROWSER",
-    "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.1; WOW64; Trident/5.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; .NET4.0C; .NET4.0E)",
-    "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; WOW64; Trident/5.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; .NET4.0C; .NET4.0E; QQBrowser/7.0.3698.400)",
-    "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; QQDownload 732; .NET4.0C; .NET4.0E)",
-    "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; Trident/4.0; SV1; QQDownload 732; .NET4.0C; .NET4.0E; 360SE)",
-    "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; QQDownload 732; .NET4.0C; .NET4.0E)",
-    "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.1; WOW64; Trident/5.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; .NET4.0C; .NET4.0E)",
-    "Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/21.0.1180.89 Safari/537.1",
-    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/21.0.1180.89 Safari/537.1",
-    "Mozilla/5.0 (iPad; U; CPU OS 4_2_1 like Mac OS X; zh-cn) AppleWebKit/533.17.9 (KHTML, like Gecko) Version/5.0.2 Mobile/8C148 Safari/6533.18.5",
-    "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:2.0b13pre) Gecko/20110307 Firefox/4.0b13pre",
-    "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:16.0) Gecko/20100101 Firefox/16.0",
-    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11",
-    "Mozilla/5.0 (X11; U; Linux x86_64; zh-CN; rv:1.9.2.10) Gecko/20100922 Ubuntu/10.10 (maverick) Firefox/3.6.10",
-    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36",
+	"Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; AcooBrowser; .NET CLR 1.1.4322; .NET CLR 2.0.50727)",
+	"Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0; Acoo Browser; SLCC1; .NET CLR 2.0.50727; Media Center PC 5.0; .NET CLR 3.0.04506)",
+	"Mozilla/4.0 (compatible; MSIE 7.0; AOL 9.5; AOLBuild 4337.35; Windows NT 5.1; .NET CLR 1.1.4322; .NET CLR 2.0.50727)",
+	"Mozilla/5.0 (Windows; U; MSIE 9.0; Windows NT 9.0; en-US)",
+	"Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Win64; x64; Trident/5.0; .NET CLR 3.5.30729; .NET CLR 3.0.30729; .NET CLR 2.0.50727; Media Center PC 6.0)",
+	"Mozilla/5.0 (compatible; MSIE 8.0; Windows NT 6.0; Trident/4.0; WOW64; Trident/4.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; .NET CLR 1.0.3705; .NET CLR 1.1.4322)",
+	"Mozilla/4.0 (compatible; MSIE 7.0b; Windows NT 5.2; .NET CLR 1.1.4322; .NET CLR 2.0.50727; InfoPath.2; .NET CLR 3.0.04506.30)",
+	"Mozilla/5.0 (Windows; U; Windows NT 5.1; zh-CN) AppleWebKit/523.15 (KHTML, like Gecko, Safari/419.3) Arora/0.3 (Change: 287 c9dfb30)",
+	"Mozilla/5.0 (X11; U; Linux; en-US) AppleWebKit/527+ (KHTML, like Gecko, Safari/419.3) Arora/0.6",
+	"Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.2pre) Gecko/20070215 K-Ninja/2.1.1",
+	"Mozilla/5.0 (Windows; U; Windows NT 5.1; zh-CN; rv:1.9) Gecko/20080705 Firefox/3.0 Kapiko/3.0",
+	"Mozilla/5.0 (X11; Linux i686; U;) Gecko/20070322 Kazehakase/0.4.5",
+	"Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.8) Gecko Fedora/1.9.0.8-1.fc10 Kazehakase/0.5.6",
+	"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.11 (KHTML, like Gecko) Chrome/17.0.963.56 Safari/535.11",
+	"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_3) AppleWebKit/535.20 (KHTML, like Gecko) Chrome/19.0.1036.7 Safari/535.20",
+	"Opera/9.80 (Macintosh; Intel Mac OS X 10.6.8; U; fr) Presto/2.9.168 Version/11.52",
+	"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.11 (KHTML, like Gecko) Chrome/20.0.1132.11 TaoBrowser/2.0 Safari/536.11",
+	"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/21.0.1180.71 Safari/537.1 LBBROWSER",
+	"Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; WOW64; Trident/5.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; .NET4.0C; .NET4.0E; LBBROWSER)",
+	"Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; QQDownload 732; .NET4.0C; .NET4.0E; LBBROWSER)",
+	"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.11 (KHTML, like Gecko) Chrome/17.0.963.84 Safari/535.11 LBBROWSER",
+	"Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.1; WOW64; Trident/5.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; .NET4.0C; .NET4.0E)",
+	"Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; WOW64; Trident/5.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; .NET4.0C; .NET4.0E; QQBrowser/7.0.3698.400)",
+	"Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; QQDownload 732; .NET4.0C; .NET4.0E)",
+	"Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; Trident/4.0; SV1; QQDownload 732; .NET4.0C; .NET4.0E; 360SE)",
+	"Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; QQDownload 732; .NET4.0C; .NET4.0E)",
+	"Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.1; WOW64; Trident/5.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; .NET4.0C; .NET4.0E)",
+	"Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/21.0.1180.89 Safari/537.1",
+	"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/21.0.1180.89 Safari/537.1",
+	"Mozilla/5.0 (iPad; U; CPU OS 4_2_1 like Mac OS X; zh-cn) AppleWebKit/533.17.9 (KHTML, like Gecko) Version/5.0.2 Mobile/8C148 Safari/6533.18.5",
+	"Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:2.0b13pre) Gecko/20110307 Firefox/4.0b13pre",
+	"Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:16.0) Gecko/20100101 Firefox/16.0",
+	"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11",
+	"Mozilla/5.0 (X11; U; Linux x86_64; zh-CN; rv:1.9.2.10) Gecko/20100922 Ubuntu/10.10 (maverick) Firefox/3.6.10",
+	"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36",
 ]
 USER_AGENT = random.choice(UA_LIST)
 
@@ -65,31 +65,36 @@ CONCURRENT_REQUESTS = 32
 
 # settings.py文件下添加mysql的配置信息
 MY_SETTINGS = {
-    "host": "39.97.186.184",
-    "user": "webset_online",
-    "passwd": "sswz2020",
-    "db": "webset_online",
-    "port": 3306,
-    "charset": "utf8mb4",
-    'use_unicode': True,
+	"host": "39.97.186.184",
+	"user": "webset_online",
+	"passwd": "sswz2020",
+	"db": "webset_online",
+	"port": 3306,
+	"charset": "utf8mb4",
+	'use_unicode': True,
 }
-
 # ############ 连接redis 信息 #################
-REDIS_URL = 'redis://:sswz2020@39.97.186.184:6379'  # 连接URL（优先于以上配置）
+REDIS_URL = 'redis://:@127.0.0.1:6379'  # 主机名
+REDIS_PORT = 6379  # 端口
+# REDIS_URL = 'redis://user:pass@hostname:9001'       # 连接URL（优先于以上配置）
 REDIS_PARAMS = {}  # Redis连接参数             默认：REDIS_PARAMS = {'socket_timeout': 30,'socket_connect_timeout': 30,'retry_on_timeout': True,'encoding': REDIS_ENCODING,}）
 # REDIS_PARAMS['redis_cls'] = 'myproject.RedisClient' # 指定连接Redis的Python模块  默认：redis.StrictRedis
 REDIS_ENCODING = "utf-8"
 # 自定义去重规则
 DUPEFILTER_CLASS = "scrapy_redis.dupefilter.RFPDupeFilter"
-# 使用scrapy-redis组件自己的调度器
-SCHEDULER = "scrapy_redis.scheduler.Scheduler"  # 核心配置
-# 是否允许暂停
-SCHEDULER_PERSIST = True  # 值为True表示：宕机恢复服务时，从宕机的那个地方开始爬取，不用从头开始
 
-# DOWNLOAD_DELAY = 3
-# The download delay setting will honor only one of:
-# CONCURRENT_REQUESTS_PER_DOMAIN = 16
-# CONCURRENT_REQUESTS_PER_IP = 16
+# 有引擎来执行：自定义调度器
+SCHEDULER = "scrapy_redis.scheduler.Scheduler"
+SCHEDULER_QUEUE_CLASS = 'scrapy_redis.queue.LifoQueue'  # 默认使用优先级队列（默认广度优先），其他：PriorityQueue（有序集合），FifoQueue（列表）、LifoQueue（列表）
+SCHEDULER_QUEUE_KEY = '%(spider)s:requests'  # 调度器中请求存放在redis中的key
+SCHEDULER_SERIALIZER = "scrapy_redis.picklecompat"  # 对保存到redis中的数据进行序列化，默认使用pickle
+SCHEDULER_PERSIST = True  # 是否在关闭时候保留原来的调度器和去重记录，True=保留，False=清空
+SCHEDULER_FLUSH_ON_START = False  # 是否在开始之前清空 调度器和去重记录，True=清空，False=不清空
+SCHEDULER_DUPEFILTER_KEY = '%(spider)s:dupefilter'  # 去重规则，在redis中保存时对应的key  chouti:dupefilter
+SCHEDULER_DUPEFILTER_CLASS = 'scrapy_redis.dupefilter.RFPDupeFilter'  # 去重规则对应处理的类
+DUPEFILTER_DEBUG = False
+REDIS_START_URLS_BATCH_SIZE = 1
+REDIS_START_URLS_AS_SET = False  # 把起始url放到redis的列表
 
 # Disable cookies (enabled by default)
 # COOKIES_ENABLED = False
@@ -112,7 +117,8 @@ SCHEDULER_PERSIST = True  # 值为True表示：宕机恢复服务时，从宕机
 # Enable or disable downloader middlewares
 # See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
 DOWNLOADER_MIDDLEWARES = {
-    'it610.middlewares.MyProxiesSpiderMiddleware': 125,
+	'it610.middlewares.MyProxiesSpiderMiddleware': 125,
+	'scrapy.downloadermiddlewares.redirect.RedirectMiddleware': 400,
 }
 
 # Enable or disable extensions
@@ -126,10 +132,10 @@ IMAGES_STORE = os.path.join(os.path.dirname(__file__), 'images')
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 ITEM_PIPELINES = {
-    'it610.pipelines.SaveImagePipeline': 1,
-    'it610.pipelines.UploadImagePipeline': 2,
-    'it610.pipelines.ItSpiderPipeline': 3,
-    'scrapy_redis.pipelines.RedisPipeline': 400
+	'it610.pipelines.SaveImagePipeline': 1,
+	'it610.pipelines.UploadImagePipeline': 2,
+	'it610.pipelines.ItSpiderPipeline': 3,
+	'scrapy_redis.pipelines.RedisPipeline': 400
 }
 # LOG_LEVEL = 'INFO'
 # Enable and configure the AutoThrottle extension (disabled by default)
@@ -153,104 +159,68 @@ ITEM_PIPELINES = {
 # HTTPCACHE_IGNORE_HTTP_CODES = []
 # HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
 IPPOOL = [
-    "176.9.75.42:3128",
-    "167.71.149.82:80",
-    "102.129.249.120:8080",
-    "39.106.223.134:80",
-    "3.211.17.212:80",
-    "46.4.96.137:8080",
-    "18.211.67.201:80",
-    "124.236.111.11:80",
-    "139.162.78.109:3128",
-    "34.193.71.122:80",
-    "180.250.12.10:80",
-    "91.205.174.26:80",
-    "113.214.13.1:1080",
-    "106.15.191.95:80",
-    "80.241.222.139:80",
-    "80.241.222.137:80",
-    "183.47.237.251:80",
-    "88.198.24.108:3128",
-    "117.185.17.16:80",
-    "117.185.17.17:80",
-    "60.255.151.82:80",
-    "221.182.31.54:8080",
-    "117.185.16.31:80",
-    "80.241.222.138:80",
-    "123.125.115.165:80",
-    "102.129.249.120:3128",
-    "47.113.96.49:80",
-    "101.132.143.232:80",
-    "112.80.248.75:80",
-    "117.185.17.144:80",
-    "117.185.17.151:80",
-    "139.224.37.83:3128",
-    "140.207.229.171:80",
-    "61.135.169.121:80",
-    "117.185.17.145:80",
-    "61.135.185.31:80",
-    "111.13.100.91:80",
-    "176.9.119.170:3128",
-    "60.255.151.81:80",
-    "116.117.134.134:82",
-    "104.250.119.186:80",
-    "123.125.114.107:80",
-    "116.117.134.134:80",
-    "112.80.248.73:80",
-    "222.74.202.228:9999",
-    "116.117.134.134:8081",
-    "180.97.34.35:80",
-    "180.149.144.224:80",
-    "122.147.141.151:80",
-    "220.181.111.37:80",
-    "5.252.161.48:8080",
-    "88.198.24.108:3128",
-    "80.241.222.137:80",
-    "104.250.119.156:80",
-    "3.211.17.212:80",
-    "176.9.75.42:3128",
-    "104.250.119.97:80",
-    "34.193.71.122:80",
-    "176.9.119.170:3128",
-    "60.255.151.82:80",
-    "46.4.96.137:8080",
-    "104.250.119.186:80",
-    "80.241.222.139:80",
-    "102.129.249.120:8080",
-    "18.211.67.201:80",
-    "117.185.17.17:80",
-    "117.185.17.16:80",
-    "122.147.141.151:80",
-    "124.236.111.11:80",
-    "117.185.16.31:80",
-    "113.214.13.1:1080",
-    "183.47.237.251:80",
-    "47.113.96.49:80",
-    "140.207.229.171:80",
-    "139.162.78.109:3128",
-    "61.135.185.31:80",
-    "103.246.250.106:80",
-    "117.185.17.151:80",
-    "112.80.248.75:80",
-    "116.117.134.134:8081",
-    "123.125.114.107:80",
-    "47.52.221.150:80",
-    "112.80.248.73:80",
-    "117.185.17.144:80",
-    "123.125.115.165:80",
-    "180.149.144.224:80",
-    "222.74.202.228:9999",
-    "180.149.144.223:80",
-    "167.71.149.82:80",
-    "60.255.151.81:80",
-    "117.185.17.145:80",
-    "202.108.22.5:80",
-    "8.210.88.234:3128",
-    "180.97.34.35:80",
-    "221.182.31.54:8080",
-    "139.224.37.83:3128",
-    "104.250.119.5:80",
-    "91.205.174.26:80",
-    "80.241.222.138:80",
-    "111.13.100.91:80",
+	"49.89.85.19:9999",
+	"222.94.145.16:3128",
+	"60.184.186.38:8888",
+	"124.205.155.158:9090",
+	"112.111.77.95:9999",
+	"1.197.203.209:9999",
+	"117.64.237.94:1133",
+	"175.42.68.192:9999",
+	"121.232.148.110:9000",
+	"121.237.24.214:3000",
+	"123.169.122.96:9999",
+	"123.101.207.207:9999",
+	"175.42.128.132:9999",
+	"59.33.58.15:9999",
+	"175.43.57.58:9999",
+	"36.249.118.62:9999",
+	"39.81.61.189:9000",
+	"115.171.202.124:9000",
+	"60.176.235.188:8888",
+	"123.163.117.68:9999",
+	"60.169.134.108	9999",
+	"175.43.152.47	9999",
+	"36.249.49.44	9999",
+	"112.111.217.36	9999",
+	"106.42.44.254	9999",
+	"123.55.114.24	9999",
+	"123.52.96.51	9999",
+	"123.163.117.68	9999",
+	"1.199.30.199	9999",
+	"110.243.26.15	9999",
+	"175.42.129.8	9999",
+	"112.111.77.96	9999",
+	"114.239.2.184	9999",
+	"1.196.177.124	9999",
+	"114.239.0.221	9999",
+	"47.106.22.156:8080",
+	"58.253.145.111:9999",
+	"59.38.61.96:9797",
+	"27.43.184.75:9999",
+	"59.36.10.79:3128",
+	"120.83.104.51:9999",
+	"119.23.238.202:3128",
+	"163.125.17.34:8888",
+	"120.76.193.51:3128",
+	"163.125.156.218:8888",
+	"58.253.158.175:9999",
+	"39.108.59.34:8118",
+	"27.43.188.228:9999",
+	"163.125.19.123:8888",
+	"219.131.240.144:9797",
+	"27.46.20.70:8888",
+	"120.83.106.112:9999",
+	"58.253.147.149:9999",
+	"120.79.184.148:8118",
+	"163.125.156.211:8888",
+	"163.125.156.213:8888",
+	"163.125.156.217:8888",
+	"14.20.235.107:9797",
+	"163.125.17.35:8888",
+	"59.33.58.15:9999",
+	"163.125.156.219:8888",
+	"27.46.20.69:8888",
+	"183.3.218.34:8080",
+	"120.83.111.116:9999",
 ]
--- it610/spiders/it610_spider.py
+++ it610/spiders/it610_spider.py
@@ -9,108 +9,117 @@ from scrapy_redis.spiders import RedisSpider
 
 
 class It610SpiderSpider(RedisSpider):
-    name = 'it610_spider'
-    allowed_domains = ['www.it610.com', 'img.it610.com']
-    start_urls = ['https://www.it610.com/']
-    faker = Faker(locale='zh_CN')
+	name = 'it610_spider'
+	allowed_domains = ['www.it610.com', 'img.it610.com']
+	start_urls = ['https://www.it610.com/']
+	faker = Faker(locale='zh_CN')
 
-    def __init__(self, *a, **kw):
-        super().__init__(*a, **kw)
-        self.imageUp = ImageUp()
+	def __init__(self, *a, **kw):
+		super().__init__(*a, **kw)
+		self.imageUp = ImageUp()
 
-    rules = (
-        Rule(LinkExtractor(allow=(r'https://www.it610.com/tags/[a-zA-Z0-9]/1.htm')), callback='parse_tags',
-             follow=True),
-    )
+	rules = (
+		Rule(LinkExtractor(allow=(r'https://www.it610.com/tags/[a-zA-Z0-9]/1.htm')), callback='parse_tags',
+		     follow=True),
+	)
 
-    def start_requests(self):
-        for url in self.start_urls:
-            yield self.make_requests_from_url(url)
+	def start_requests(self):
+		for url in self.start_urls:
+			yield self.make_requests_from_url(url)
 
-    def make_requests_from_url(self, url):
-        return Request(url=str("https://www.it610.com/"), callback=self.parse_tags, dont_filter=True, )
+	def make_requests_from_url(self, url):
+		return Request(url=str("https://www.it610.com/"), callback=self.parse_tags, dont_filter=True, )
 
-    def parse_tags(self, response):
-        linkExt = LinkExtractor(allow=r'https://www.it610.com/tags/[a-zA-Z0-9]/1.htm')
-        links = linkExt.extract_links(response)
-        if links:
-            for link in links:
-                request = Request(str(link.url), callback=self.parse_search, dont_filter=True,
-                                  headers={'Connection': 'close', 'refer': str(response.url)})
-                yield request
+	def parse(self, response):
+		linkExt = LinkExtractor(allow=r'https://www.it610.com/tags/[a-zA-Z0-9]/1.htm')
+		links = linkExt.extract_links(response)
+		if links:
+			for link in links:
+				request = Request(str(link.url), callback=self.parse_search, dont_filter=True,
+				                  headers={'Connection': 'close', 'refer': str(response.url)})
+				yield request
 
-    def parse_search(self, response):
-        linkExt = LinkExtractor(allow=r'https://www.it610.com/search/.*/1.htm')
-        links = linkExt.extract_links(response)
-        if links:
-            for link in links:
-                request = Request(str(link.url), callback=self.parse_article, dont_filter=True,
-                                  headers={'Connection': 'close', 'refer': str(response.url)})
-                yield request
-        next_page_str = response.xpath(
-            '//div[contains(@class,"container") and contains(@class,"mt10")]/div[contains(@class,"page_mod") and contains(@class,"mt15")]/span[@class="page_next"]/a/@href').get()
-        if next_page_str:
-            next_page = "https://www.it610.com" + next_page_str
-            Request(next_page, callback=self.parse_search,
-                    headers={'Connection': 'close', 'refer': str(response.url)})
+	def parse_tags(self, response):
+		linkExt = LinkExtractor(allow=r'https://www.it610.com/tags/[a-zA-Z0-9]/1.htm')
+		links = linkExt.extract_links(response)
+		if links:
+			for link in links:
+				request = Request(str(link.url), callback=self.parse_search, dont_filter=True,
+				                  headers={'Connection': 'close', 'refer': str(response.url)})
+				yield request
 
-    def parse_article(self, response):
-        linkExt = LinkExtractor(allow=r'/article/.*.htm')
-        links = linkExt.extract_links(response)
-        if links:
-            for link in links:
-                url = link.url.replace("https://www.it610.com", "")
-                xpath_str = '//a[contains(@href,"%s")]/div[@class="article-excerpt"]' % url
-                article_summary = response.xpath(xpath_str).getall()
-                request = Request(str(link.url), callback=self.parse_article_detail,
-                                  meta={"article_summary": "".join(article_summary)},
-                                  headers={'Connection': 'close', 'refer': str(response.url)})
-                yield request
-        next_page_str = response.xpath(
-            '//div[contains(@class,"container") and contains(@class,"mt10")]/div[contains(@class,"page_mod") and contains(@class,"mt15")]/span[@class="page_next"]/a/@href').get()
-        if next_page_str:
-            next_page = "https://www.it610.com" + next_page_str
-            Request(next_page, callback=self.parse_article,
-                    headers={'Connection': 'close', 'refer': str(response.url)})
+	def parse_search(self, response):
+		linkExt = LinkExtractor(allow=r'https://www.it610.com/search/.*/1.htm')
+		links = linkExt.extract_links(response)
+		if links:
+			for link in links:
+				request = Request(str(link.url), callback=self.parse_article, dont_filter=True,
+				                  headers={'Connection': 'close', 'refer': str(response.url)})
+				yield request
+		next_page_str = response.xpath(
+			'//div[contains(@class,"container") and contains(@class,"mt10")]/div[contains(@class,"page_mod") and contains(@class,"mt15")]/span[@class="page_next"]/a/@href').get()
+		if next_page_str:
+			next_page = "https://www.it610.com" + next_page_str
+			Request(next_page, callback=self.parse_search,
+			        headers={'Connection': 'close', 'refer': str(response.url)})
 
-    def parse_article_detail(self, response):
-        article_itemLoader = ArticleItemLoader(item=ItSpiderItem(), response=response)
-        article_summary = response.meta.get("article_summary")
-        if article_summary:
-            article_itemLoader.add_value("article_summary", article_summary)
-        else:
-            article_itemLoader.add_value("article_summary", 'article_summary')
+	def parse_article(self, response):
+		linkExt = LinkExtractor(allow=r'/article/.*.htm')
+		links = linkExt.extract_links(response)
+		if links:
+			for link in links:
+				url = link.url.replace("https://www.it610.com", "")
+				xpath_str = '//a[contains(@href,"%s")]/div[@class="article-excerpt"]' % url
+				article_summary = response.xpath(xpath_str).getall()
+				request = Request(str(link.url), callback=self.parse_article_detail,
+				                  meta={"article_summary": "".join(article_summary)},
+				                  headers={'Connection': 'close', 'refer': str(response.url)})
+				yield request
+		next_page_str = response.xpath(
+			'//div[contains(@class,"container") and contains(@class,"mt10")]/div[contains(@class,"page_mod") and contains(@class,"mt15")]/span[@class="page_next"]/a/@href').get()
+		if next_page_str:
+			next_page = "https://www.it610.com" + next_page_str
+			Request(next_page, callback=self.parse_article,
+			        headers={'Connection': 'close', 'refer': str(response.url)})
 
-        public_title = response.xpath('//div/h1[@id="articleTitle"]/text()').get()
-        if public_title:
-            article_itemLoader.add_value("title", public_title)
+	def parse_article_detail(self, response):
+		article_itemLoader = ArticleItemLoader(item=ItSpiderItem(), response=response)
+		article_summary = response.meta.get("article_summary")
+		if article_summary:
+			article_itemLoader.add_value("article_summary", article_summary)
+		else:
+			article_itemLoader.add_value("article_summary", 'article_summary')
 
-        public_anthor = response.xpath(
-            '//div[@class="article__authorright"]/div[@class="article__authormeta"]/a/strong/text()').get()
-        if public_anthor:
-            article_itemLoader.add_value("author", public_anthor)
-        else:
-            randomName = self.faker.name()
-            article_itemLoader.add_value("author", randomName)
+		public_title = response.xpath('//div/h1[@id="articleTitle"]/text()').get()
+		if public_title:
+			article_itemLoader.add_value("title", public_title)
 
-        public_time = response.xpath('//div[@class="article__authorright"]//span/text()').get()
-        if public_time:
-            article_itemLoader.add_value("pub_time", public_time)
-        else:
-            article_itemLoader.add_value("pub_time", "")
+		public_anthor = response.xpath(
+			'//div[@class="article__authorright"]/div[@class="article__authormeta"]/a/strong/text()').get()
+		if public_anthor:
+			article_itemLoader.add_value("author", public_anthor)
+		else:
+			randomName = self.faker.name()
+			article_itemLoader.add_value("author", randomName)
 
-        article_itemLoader.add_value("origin_url", response.url)
-        article_itemLoader.add_value("article_id", response.url)
+		public_time = response.xpath('//div[@class="article__authorright"]//span/text()').get()
+		if public_time:
+			article_itemLoader.add_value("pub_time", public_time)
+		else:
+			article_itemLoader.add_value("pub_time", "")
 
-        public_content = response.xpath('//div[@class="markdown_views"]/div[@id="article_content"]').get()
-        if public_content:
-            article_itemLoader.add_value("content", public_content)
+		article_itemLoader.add_value("origin_url", response.url)
+		article_itemLoader.add_value("article_id", response.url)
 
-        public_subject = response.xpath('//ul[contains(@class,"taglist--inline")]/li/a[@class="tag"]/text()').get()
-        if public_subject:
-            article_itemLoader.add_value("subject", public_subject.strip())
-        else:
-            article_itemLoader.add_value("subject", 0)
+		public_content = response.xpath('//div[@class="markdown_views"]/div[@id="article_content"]').get()
+		if public_content:
+			article_itemLoader.add_value("content", public_content)
 
-        item = article_itemLoader.load_item()
-        yield item
+		public_subject = response.xpath('//ul[contains(@class,"taglist--inline")]/li/a[@class="tag"]/text()').get()
+		if public_subject:
+			article_itemLoader.add_value("subject", public_subject.strip())
+		else:
+			article_itemLoader.add_value("subject", 0)
+
+		item = article_itemLoader.load_item()
+		yield item
